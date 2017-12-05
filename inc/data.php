940ec28c62f5183a67d22a8b5cf5215082b625bd
elvis <34288577+elvist69@users.noreply.github.com>
2017-12-05 18:43:48 -0500

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,170 @@
+<?php
+$catalog = [];
+//books
+$catalog[101] = [
+"title" => "Tears Of A Tiger",
+"img" => "img/media/book1.jpeg",
+    "genre" => "Realistic Fiction",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "Sharon M Draper",
+    ],
+    "publisher" => "Atheneum",
+    "isbn" => '978-0-689-80698-8'
+];
+$catalog[102] = [
+"title" => "Fahrenhit 451",
+"img" => "img/media/book2.jpeg",
+    "genre" => "Dystopian",
+    "format" => "Paperback",
+    "year" => 1953,
+    "category" => "Books",
+    "authors" => [
+        "Ray Bradbury"
+    ],
+    "publisher" => "Ballantine Books",
+    "isbn" => '978-0-7432-4722-1'
+];
+$catalog[103] = [
+"title" => "The Outsiders",
+"img" => "img/media/book3.jpeg",
+    "genre" => "Young Adult Fiction",
+    "format" => "Paperback",
+    "year" => 1967,
+    "category" => "Books",
+    "authors" => [
+        "S.E Hinton"
+    ],
+    "publisher" => "Viking Press",
+    "isbn" => '0-670-53257-6'
+];
+$catalog[104] = [
+"title" => "As I Descended",
+"img" => "img/media/book4.jpeg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2016,
+    "category" => "Books",
+    "authors" => [
+        "Robin Talley"
+    ],
+    "publisher" => "HarperCollins Publishers",
+    "isbn" => '9780062409232'
+];
+
+//Movies
+$catalog[201] = [
+    "title" => "Kick-Ass",
+    "img" => "img/media/movie1.jpeg",
+    "genre" => "Fiction",
+    "format" => "DVD",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "Matthew Vaughn",
+    "writers" => [
+        "Jane Goldman",
+        "Matthew Vaughn"
+    ],
+    "stars" => [
+        "Chole Grace Moretz",
+        "Aaron Taylor-Johnson",
+        "Nicolas Cage",
+        "Evan Peters"
+    ]
+];
+$catalog[202] = [
+    "title" => "Star Wars - Revenge of the Sith",
+    "img" => "img/media/movie2.jpeg",
+    "genre" => "Fantasy",
+    "format" => "DVD",
+    "year" => 2005,
+    "category" => "Movies",
+    "director" => "George Lucas",
+    "writers" => [
+        "George Lucas"
+    ],
+    "stars" => [
+        "Natalie Portman",
+        "Hayden Christensen",
+        "Ewan McGregor",
+        "Samuel L Jackson"
+    ]
+];
+$catalog[203] = [
+    "title" => "Star Wars - The Force Awakens",
+    "img" => "img/media/movie3.jpeg",
+    "genre" => "Fantasy",
+    "format" => "DVD",
+    "year" => 2015,
+    "category" => "Movies",
+    "director" => "J.J Abrams",
+    "writers" => [
+        "J.J Abrams"
+    ],
+    "stars" => [
+        "Daisy Ridley",
+        "John Boyega",
+        "Harrison Ford",
+        "Carrie Fisher"
+    ]
+];
+$catalog[204] = [
+    "title" => "Star Wars - Empire Strikes Back",
+    "img" => "img/media/movie4.jpeg",
+    "genre" => "Fantasy",
+    "format" => "DVD",
+    "year" => 1980,
+    "category" => "Movies",
+    "director" => "George Lucas",
+    "writers" => [
+        "George Lucas"
+    ],
+    "stars" => [
+        "Mark Hamill",
+        "Peter Mayhew",
+        "Harrison Ford",
+        "Carrie Fisher"
+    ]
+];
+
+//Music
+$catalog[301] = [
+    "title" => "Slipknot: Welcome to our Neighborhood",
+    "img" => "img/media/music1.jpeg",
+    "genre" => "Metal",
+    "format" => "CD",
+    "year" => 1999,
+    "category" => "Music",
+    "artist" => "Slipknot"
+];
+$catalog[302] = [
+    "title" => "Papa Roach: The Paramour Sessions",
+    "img" => "img/media/music2.jpeg",
+    "genre" => "Alternative Rock",
+    "format" => "CD",
+    "year" => 2006,
+    "category" => "Music",
+    "artist" => "Papa Roach"
+];
+$catalog[303] = [
+    "title" => "Bullet For My Valentine: The Poison",
+    "img" => "img/media/music3.jpeg",
+    "genre" => "Heavy Metal",
+    "format" => "CD",
+    "year" => 2005,
+    "category" => "Music",
+    "artist" => "Bullet For My Valentine"
+];
+$catalog[304] = [
+    "title" => "Asking Alexandria: The Black",
+    "img" => "img/media/music4.jpeg",
+    "genre" => "Metalcore",
+    "format" => "CD",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Asking Alexandria"
+];
+
+ ?>
